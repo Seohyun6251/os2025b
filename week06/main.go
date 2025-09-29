51f5a6554cab6759e9b16e2d1444f99851eb209b
sinome2006 <sinome2006@gmail.com>
2025-09-29 14:16:18 +0900

    v0.2 types/main.go

--- week06/main.go
+++ week06/main.go
@@ -6,7 +6,10 @@ import (
 	"strings"
 )
 
+// ctrl + F5 실행
 func main() {
 	fmt.Println(math.Ceil(2.31))
 	fmt.Println(strings.Title("go developer~"))
+	fmt.Println("Kim\nInha\t\"\\") // C like
+	fmt.Println('A', '가')
 }
